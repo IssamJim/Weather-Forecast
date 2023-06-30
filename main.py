f79ea53f1f3fa9373c60c0b159a32311b8fa3022
Issam Silverio Jimenez Ortega <issamjimenez.98@gmail.com>
2023-06-30 14:44:30 -0700

    smn

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 
+# Agregamos titulo, text input, slider, selectbox and subheader
 place = st.text_input("Place: ")
 
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -14,9 +15,27 @@ option = st.selectbox("Select data to view",
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+if place:
+    # Temperatura y datos del cielo
+    try:
+        filtered_data = get_data(place, days)
 
-d,t = get_data(days)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            
+            dates = [dict["dt_txt"] for dict in filtered_data]
+        #create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Sky":
+        
+            images = {"Clear":"images/clear.png", "Clouds": "images/cloud.png",
+                    "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write("Ese lugar no esta en la base de datos.")
